ff03035ab981587ed1c2bc623a29ef92c614884c
wuyanqiang <461804160@qq.com>
2019-04-11 09:41:20 +0800

    手动刷新配置 done

--- eureka-client/src/main/java/com/zq/HelloController.java
+++ eureka-client/src/main/java/com/zq/HelloController.java
@@ -1,10 +1,12 @@
 package com.zq;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope //POST 方式请求地址：http://localhost:{port}/actuator/refresh
 public class HelloController {
 
     @Value("${server.port}")
